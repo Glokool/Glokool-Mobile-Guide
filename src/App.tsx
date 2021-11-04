49132f9a008e6275b4e90d7f1aa2d212a62a90d7
trumanfromkorea <jangjh971008@naver.com>
2021-11-04 14:22:09 +0900

    add: splash screen, android 앱 두번 설치 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import * as eva from '@eva-design/eva';
-import auth from '@react-native-firebase/auth';
-
-import { Provider, useDispatch } from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './model';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
@@ -13,55 +11,57 @@ import SplashScreen from 'react-native-splash-screen';
 import { AuthContext } from './context';
 import messaging from '@react-native-firebase/messaging';
 import { authContextType } from './context/AuthContext';
-import { Alert, StatusBar } from 'react-native';
-import { SERVER } from './server';
-import axios from 'axios';
+import { StatusBar, LogBox } from 'react-native';
 
 
 // 백그라운드 메시지 리스너
 messaging().setBackgroundMessageHandler(async remoteMessage => {
-  console.log('Message handled in the background!', remoteMessage);
+    console.log('Message handled in the background!', remoteMessage);
 });
 
 
 export default function App() {
 
-  // Redux Store 파일
-  const store = createStore(rootReducer);
+    // Redux Store 파일
+    const store = createStore(rootReducer);
 
-  // Firebase 상태 관리
-  const [currentUser, setCurrentUser] = React.useState<authContextType | null>(null);
-  const userValue = { currentUser, setCurrentUser };
+    // Firebase 상태 관리
+    const [currentUser, setCurrentUser] = React.useState<authContextType | any>(null);
+    const userValue = { currentUser, setCurrentUser };
 
-  React.useEffect(() => {
-    // 포어그라운드 메시지 리스너
-    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+    React.useEffect(() => {
+        // 포어그라운드 메시지 리스너
+        const unsubscribe = messaging().onMessage(async (remoteMessage) => {
 
-    });
+        });
 
-    setTimeout(() => {
-      SplashScreen.hide();
-    }, 500)
+        setTimeout(() => {
+            SplashScreen.hide();
+        }, 500)
 
-    // 앱 종료시 실행 함수
-    return () => {
-      unsubscribe;
-    }
-  }, [])
+        // 앱 종료시 실행 함수
+        return () => {
+            unsubscribe;
+        }
+    }, [])
 
 
-  return (
-    <Provider store={store}>
-      <IconRegistry icons={EvaIconsPack} />
-      <StatusBar barStyle="dark-content" />
-      <ApplicationProvider {...eva} theme={{ ...eva.light }} >
-        <AuthContext.Provider value={userValue}>
-          <NavigationContainer>
-            <AppNavigator key={'APP'} type={React.Component} props={undefined} />
-          </NavigationContainer>
-        </AuthContext.Provider>
-      </ApplicationProvider>
+    return (
+        <Provider store={store}>
+            <IconRegistry icons={EvaIconsPack} />
+            <StatusBar barStyle="dark-content" />
+            <ApplicationProvider {...eva} theme={{ ...eva.light }} >
+                <AuthContext.Provider value={userValue}>
+                    <NavigationContainer>
+                        <AppNavigator key={'APP'} type={React.Component} props={undefined} />
+                    </NavigationContainer>
+                </AuthContext.Provider>
+            </ApplicationProvider>
 
-    </Provider>
-  );
+        </Provider>
+    );
 }
+
+LogBox.ignoreLogs([
+    "EventEmitter.removeListener('change', ...)"
+]);
